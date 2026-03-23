d2a7f5a4b46e1547ba0e4eb2919d6445d3fb8215
Krunal <krunalkolhe0409@gmail.com>
2026-03-23 14:10:51 +0530

    changes made

--- main.py
+++ main.py
@@ -1,2 +1,17 @@
+from flask import Flask
+
 print("Successfully make docker image")
 print(" :-)   we can do anything now hurray!!!!!")
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello():
+    return "Successfully made Docker image! :-) We can do anything now hurray!!!!!"
+
+@app.route('/health')
+def health():
+    return {"status": "healthy"}, 200
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000, debug=False)
